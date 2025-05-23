c9cf83747b7678435dc9579e4f1de40c6fc294e5
micme256 <nshimye.michael.2019@gmail.com>
2025-05-23 17:18:24 +0300

    Added send loan confirmation request to treasurer after loan approval

--- appscript/main-edit-data.js
+++ appscript/main-edit-data.js
@@ -6,6 +6,10 @@ const editData = (formData) => {
     const { data, sheet } = checkSheetData(transactionType);
     const headers = data[0];
 
+    if (formData.transactionDate) {
+      formData.transactionDate = convertToDate(formData.transactionDate);
+    }
+
     let updated = false;
     const edittedData = {};
 
@@ -30,8 +34,43 @@ const editData = (formData) => {
     if (!updated) {
       throw new Error(`Transaction ID '${transactionId}' not found`);
     }
+    //Request treasurer confirmation after loan approval
+    if (
+      formData.transactionType === "newLoan" &&
+      formData.status === "approved"
+    ) {
+      const members = namesMapedById();
+      const memberName = members.get(formData.memberId);
+      edittedData.memberName = memberName;
+      secondApprovalEmail(edittedData);
+    }
     return edittedData;
   } catch (error) {
     return error;
   }
+};
+
+const secondApprovalEmail = (requestOptions) => {
+  const { memberName, memberId, transactionId } = requestOptions;
+  const email = "michaelnshimye@gmail.com";
+  const subject = "New loan approved for Mr. " + memberName;
+
+  const approveUrl =
+    "https://script.google.com/macros/s/AKfycbyXfGOmWQDMmopVQcfpMlVEFU8PoDbzxltLIhtHZG3mKR7PJ_rfE_a-7n1dpwpYxxLx/exec";
+  const body = `
+Hello,
+
+A new loan request has been approved and is pending your confirmation. Please go on and disburse the amount to the applicant and confirm the transaction.
+
+LOAN  DETAILS:
+- Transaction ID: ${transactionId}
+- Member ID: ${memberId}
+
+Click below to disburse the approved amount and confirm issuance of this loan:
+
+${approveUrl}
+
+Regards,  
+NEON management system`;
+  GmailApp.sendEmail(email, subject, body);
 };
