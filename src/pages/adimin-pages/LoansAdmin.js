2eb9fab08e363f97a9d8a7596d01b9f1a2b8d200
micme256 <nshimye.michael.2019@gmail.com>
2025-05-23 17:38:51 +0300

    Removed loan activity and placed it into outlet

--- src/pages/adimin-pages/LoansAdmin.js
+++ src/pages/adimin-pages/LoansAdmin.js
@@ -1,19 +1,9 @@
-import RecentActivityAdmn from "../../components/admin-pages-comps/RecentActivityAdmn";
 import KeyMetrics from "../../components/KeyMetrics";
-import useFetchData from "../../components/hooks/useFetchData";
 import { useGeneralData } from "../../components/admin-pages-comps/GeneralDataContext";
 import AdminNav from "./AdminNav";
+import { Outlet } from "react-router-dom";
 
 const LoansAdmin = () => {
-  const {
-    data: transactions,
-    loading,
-    error,
-  } = useFetchData({
-    dataType: "transactions",
-    transactionType: "loans",
-    limit: 20,
-  });
   const { metricObj } = useGeneralData();
   const metricsData = metricObj
     ? {
@@ -22,34 +12,10 @@ const LoansAdmin = () => {
       }
     : {};
 
-  if (loading) {
-    return (
-      <>
-        <KeyMetrics metricsData={metricsData} />
-        <p className="loading">Loading...</p>
-        <AdminNav />
-      </>
-    );
-  }
-
-  if (error) {
-    return (
-      <>
-        <KeyMetrics metricsData={metricsData} />
-        <p className="error">Error: {error}</p>
-        <AdminNav />
-      </>
-    );
-  }
-
   return (
     <>
       <KeyMetrics metricsData={metricsData} />
-      {transactions && transactions.length > 0 ? (
-        <RecentActivityAdmn recentActivity={transactions} />
-      ) : (
-        <p>No loan transactions found.</p>
-      )}
+      <Outlet />
       <AdminNav />
     </>
   );
